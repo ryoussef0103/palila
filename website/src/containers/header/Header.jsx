811a86e16df1a9694795442950374fcb85f67b01
Robert <ryoussef0103@hotmail.com>
2022-08-03 21:42:21 +0200

    more media queries fixes

--- website/src/containers/header/Header.jsx
+++ website/src/containers/header/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 // import palilaPoster from '../../assets/palilaPoster.svg'
 import './header.css'
+import Background from '../../assets/DesktopView.svg'
 
 
 
 const Header = () => {
   return (
-
-    <div className="palila__header" >
-  
+    <div className="palila__header text-center text-white d-flex" 
+    style={{backgroundImage: `url(${Background})`,
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '1276px'
+  }} 
+    >
       <div className="palila__header-content">
         <h1>Text</h1>
       </div>
